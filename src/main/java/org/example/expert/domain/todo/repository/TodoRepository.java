0816fd959ad730a5dba474433e26a9876b72b5fc
soo59599 <sooh59599@gmail.com>
2025-09-10 20:37:08 +0900

    lv1: 3. 코드 개선 퀴즈 -  JPA의 이해 fix

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,9 +16,9 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
+    @EntityGraph(attributePaths = {"user"})
     @Query("""
             SELECT t FROM Todo t
-            LEFT JOIN FETCH t.user u
                     WHERE (:weather IS NULL OR t.weather = :weather)
                             AND(:startDate IS NULL OR t.modifiedAt >= :startDate)
                             AND(:endDate IS NULL OR t.modifiedAt <= :endDate)
