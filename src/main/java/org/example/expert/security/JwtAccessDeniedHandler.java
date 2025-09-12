d33350714283ef7cb264cd16c7decf7c980c6f6e
soo59599 <sooh59599@gmail.com>
2025-09-12 22:36:32 +0900

    lv2: 9. Spring Security

---
+++ src/main/java/org/example/expert/security/JwtAccessDeniedHandler.java
@@ -0,0 +1,39 @@
+package org.example.expert.security;
+
+import ch.qos.logback.core.spi.ErrorCodes;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+import jdk.jshell.Snippet.Status;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    // 403 권한 없음
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        HttpStatus status = HttpStatus.FORBIDDEN;
+        response.setStatus(status.value());
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        Map<String, Object> errorMap = SecurityErrorResponseUtil.getErrorMap(status, "관리자 권한이 없습니다.");
+
+        response.getWriter().write(objectMapper.writeValueAsString(errorMap));
+
+    }
+}
